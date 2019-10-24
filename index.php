e437625e50ac36313c2b54ad41123000ff39bcaa
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-10-24 18:44:31 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// print_r(PDO::getAvailableDrivers()); //to see the list of drivers that are available for you to use. on my machine, it is mysql: version=sqlite.
+
+// now to connect to the database that is hosted in the above driver.
+
+// $connection = new PDO('mysql:host=localhost;dbname=rush', 'root', ''); //MYSQL=driver; HOST=your server host, for now we are dealing with localhost(127.0.0.1); dbname=whichever database you will be connecting to \
+                                                       // and accessing; 'user' then 'password'. should any of the above entries be wrong, a connection will fail to establish and thus you may get, the page does not exist
+
+// now for error mode
+// we will use the try-catch block
+try
+{
+    $connection = new PDO('mysql:host=localhost;dbname=rush', 'root', '');
+}
+// TRY will try to run whatever code is in the TRY block, if it fails, the CATCH block will catch that error or exception and instead of crashing the page ito \
+// the error/exception, it will instead execute the code that is in the CATCH block.
+catch(PDOException $some_exception) //in this case, the EXCEPTION/ERROR that is caught, is stored inside the variable $SOME_EXCEPTION or which ever variable that you have declared here.
+{
+    echo $some_exception->getMessage()."</br>";
+    die('password?');
+}
+    // catch() catches the error that has occurred and do whatever corrective measure you direct it to do.
+    // for instance, if you have inputted the wrong database, user, password.
+    // in this case, die() is used as a corrective measure to ensure that the error will not cause the page to stop working without warning. also to make sure that \
+    // the program stops on our terms.
+
+    // IF THERE IS NO EXCEPTION CAUGHT.
+
+    echo "OK! we are good! </br>";
+?>
