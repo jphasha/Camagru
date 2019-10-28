d71abc5909e4e8e68e9924defa5815b7e9e021a9
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-10-28 18:36:09 +0200

    core/initialise.php not clear as to what is happening here

--- core/initialise.php
+++ core/initialise.php
@@ -1,11 +1,38 @@
 <?php
+// for now it is in everybody's best interest if we don't talk about this file.
 session_start(); //allows for people to login. without a session, login is impossible.
 
-//now we are creating a config class which will allow us to
+//now we are creating a config class which will allow us to:
 $GLOBALS['config'] = array
 (
-    'mysql' => array(),
-    'remember' => array(),
-    'sessions' => array()
-)
+    // connect to our database.
+    'mysql' => array
+    (
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'db' => 'db_jphasha'
+    ),
+    // setup cookies.
+    'remember' => array
+    (
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 3600
+    ),
+    // setup a session.
+    'sessions' => array
+    (
+        'session' => 'user'
+    )
+);
+
+spl_autoload_register
+(
+    function($class)
+    {
+        require_once 'classes/' . $class . '.php';
+    }
+);
+
+require_once 'functions/sanitize.php';
 ?>
