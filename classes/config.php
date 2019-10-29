46e2f12bd175fd6956b035303cf130e1f164e07c
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-10-29 18:34:45 +0200

    this may take a while

--- classes/config.php
+++ classes/config.php
@@ -8,17 +8,25 @@ class Config
             // properties / variables
             $config = $GLOBALS['config']; //remember, $GLOBALS is a global variable so it can be called without requiring the file it is declared in(i think).
             $path = explode('/', $path);
-            print_r($GLOBALS);
-            echo "<br>";
-            print_r($path);
-            echo "<br>";
+            // print_r($GLOBALS);
+            // echo "<br>";
+            // print_r($path);
+            // echo "<br>";
 
             // now printing the above arrays in a while loop.
             foreach ($path as $element)
             {
                 echo $element."<br>";
+                if (isset($config[$element]))
+                {
+                    echo ":".$element."<br>";
+                }
+                else
+                {
+                    echo ": not an element <br>";
+                }
             }
-            // don't know how to break arrays 
+            
         }
     }
 }
