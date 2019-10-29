ac02d850a638546bd258a332f4fe8cb6ac45bb3a
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-10-29 18:20:55 +0200

    hang on, i think i am getting something

--- classes/config.php
+++ classes/config.php
@@ -5,11 +5,20 @@ class Config
     {
         if ($path)
         {
-            $config = $GLOBALS['config'];
+            // properties / variables
+            $config = $GLOBALS['config']; //remember, $GLOBALS is a global variable so it can be called without requiring the file it is declared in(i think).
             $path = explode('/', $path);
             print_r($GLOBALS);
             echo "<br>";
             print_r($path);
+            echo "<br>";
+
+            // now printing the above arrays in a while loop.
+            foreach ($path as $element)
+            {
+                echo $element."<br>";
+            }
+            // don't know how to break arrays 
         }
     }
 }
--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 require_once 'core/initialise.php';
 
 echo Config::get('mysql/host/phasha'); //"::" the double colons are the "->" sccessing method to access a static function (or a variable).
+// basically, right now i'm just calling the function get from the class Config.
 ?>
