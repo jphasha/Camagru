b3abed9a0560cbef8304f2d8ec8a99ba76136122
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-11-20 13:38:19 +0200

    view_gal

--- view_gal.php
+++ view_gal.php
@@ -23,7 +23,7 @@ $images = $gallery->getImages(array('png')); // it is possible that i don't quit
             <div class="gallery cf">
                 <?php foreach($images as $image): ?>
                     <div class="gal_item">
-                        <a href="<?php echo $image['full'] ?>"><img src="<?php echo $image['thumbs']; ?>" class="pre_img"> 
+                        <a href="<?php echo $image['full'] ?>"><img src="<?php echo $image['full']; ?>" class="pre_img"> 
                     </div>
                 <?php endforeach; ?>
             </div>
