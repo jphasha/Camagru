ef64c19553b5b2c09aa2d149b90cf82c5321822e
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-11-21 14:55:01 +0200

    introducing the like and comments sections

--- view_gal.php
+++ view_gal.php
@@ -23,7 +23,14 @@ $images = $gallery->getImages(array('png')); // it is possible that i don't quit
             <div class="gallery cf">
                 <?php foreach($images as $image): ?>
                     <div class="gal_item">
-                        <a href="<?php echo $image['full'] ?>"><img src="<?php echo $image['full']; ?>" class="pre_img"> 
+                        <a href="<?php echo $image['full'] ?>"><img src="<?php echo $image['full']; ?>" class="pre_img">
+                        <div class="like_field">
+                            <a href="">Like</a>
+                            <p>3</p>
+                        </div>
+                        <div class="comment_field">
+                            <a href="">Comment</a>
+                        </div>
                     </div>
                 <?php endforeach; ?>
             </div>
