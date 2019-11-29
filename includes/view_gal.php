1b26eb68e520eb633477932b9e080e67d979a3de
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-11-29 17:03:41 +0200

    public vs private

--- includes/view_gal.php
+++ includes/view_gal.php
@@ -2,12 +2,57 @@
 require '../core/initialise.php';
 
 $gallery = new Gallery();
+$user = new User();
 $gallery->setPath('../uploads/');
 
-$images = $gallery->getImages(array('png')); // it is possible that i don't quite this getImages function because it is misbehaving.
+$images = $gallery->getImages(array()); 
 
+if (!$user->isLoggedIn())
+{
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Camagru</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+    </header>
+    <div class="gal_con">
+        <?php if($images): ?>
+            <div class="gallery cf">
+                <?php foreach($images as $image): ?>
+                    <div class="gal_item">
+                        <a href="<?php echo $image['full'] ?>"><img src="<?php echo $image['full']; ?>" class="pre_img">
+                        <div class="like_field">
+                            <p>3</p>
+                        </div>
+                        <div class="comment_field">
+                            <a href="">Comment</a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        <?php else: ?>
+            There are no images.
+        <?php endif; ?>
+    </div>
+    <footer class="footer">
+    &copy; jphasha 2019
+    </footer>
+</body>
+</html>
+<?php
+}
+
+else if ($user->isLoggedIn())
+{
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,3 +91,6 @@ $images = $gallery->getImages(array('png')); // it is possible that i don't quit
     </footer>
 </body>
 </html>
+<?php
+}
+?>
