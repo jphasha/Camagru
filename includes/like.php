36bdd00cac1850aed430033cce126b13c00907ef
Johannes PHASHA <jphasha@c5r2s2.wethinkcode.co.za>
2019-11-30 17:47:13 +0200

    small progress2

--- includes/like.php
+++ includes/like.php
@@ -6,6 +6,8 @@ error_reporting(E_ALL);
 
 require_once '../core/initialise.php';
 
+$db = DB::getInstance();
+
 if (isset($_GET['type'], $_GET['id']))
 {
     $type = $_GET['type'];
@@ -15,10 +17,27 @@ if (isset($_GET['type'], $_GET['id']))
     {
         case 'file':
             $db->query(
-            "INSERT INTO likes (picture_id, liker_id) 
-            SELECT {}, {Session::get('name')}"
-            );
+            "INSERT INTO likes (picture_id, liker_id) /* insert picture_id and user_id into likes table */ 
+            SELECT {$id}, {$_SESSION['user_id']}
+            FROM pictures
+            WHERE EXISTS /* the picture must exist in the pictures table and have the same id as the one being liked */
+            (
+                SELECT picture_id
+                FROM pictures
+                WHERE picture_id = {$id}
+            )
+            AND NOT EXISTS /* however, data of this pic the user, must not already be in the likes table together. */
+            (
+                SELECT like_id
+                FROM likes
+                WHERE user = {$_SESSION['user_id']}
+                AND picture = {$id}
+            )
+            LIMIT 1    
+            ");
         break;
     }
 }
+
+Redirect::to('../index.php');
 ?>
